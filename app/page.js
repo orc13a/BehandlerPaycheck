98ed645d99b4a6a3ba87b6ccc72a041f932416c8
Oliver Christensen <35222102+orc13a@users.noreply.github.com>
2023-10-28 02:41:37 +0200

    change

--- app/page.js
+++ app/page.js
@@ -1,8 +1,34 @@
 // import styles from './page.module.css'
+import Image from "next/image";
+import { Box, Button, Container, Link, Typography } from "@mui/joy";
 
 export default function Home() {
-  return (
-    <>
-    </>
-  )
+    return (
+        <>
+            <Container sx={{ height: '100%' }}>
+                <Box className='flying'>
+                    <Link href='/calculator'>
+                        {/* <Typography variant="solid" color="primary" noWrap>
+                            udregn løn
+                        </Typography> */}
+                        <Button color='neutral' size='lg'>
+                            udregn løn
+                        </Button>
+                    </Link>
+                </Box>
+                <Box sx={{ height: '100%' }}>
+                    <Box sx={{ height: '100%' }}>
+                        <Typography className="infoText">
+                            Lønsedler kan være sværre og forvirrende at forstå. Du er i tvivl om lønsedlen er rigtigt og om du den løn du har arbejdet for.
+                        </Typography>
+                    </Box>
+                    <Box className='homeSvgBox'>
+                        <span>
+                            Få den løn du fortjener
+                        </span>
+                    </Box>
+                </Box>
+            </Container>
+        </>
+    )
 }
